f8c60f5eabeedb40b6ea592d9c2c350593b95cca
Sylwia Bartula <sylwinka21@tlen.pl>
2019-11-17 16:41:01 +0100

    Updates

---
+++ precache-manifest.4b3bc606a8ef537181ca9e82f1b624e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ecdc84f07fdac44cce8de5522d2e9a",
+    "url": "/bitcoin-react/index.html"
+  },
+  {
+    "revision": "0a7bfbb14a6419648285",
+    "url": "/bitcoin-react/static/css/main.9596b852.chunk.css"
+  },
+  {
+    "revision": "9e37f715c8c8b22c14d8",
+    "url": "/bitcoin-react/static/js/2.47ea83c7.chunk.js"
+  },
+  {
+    "revision": "0a7bfbb14a6419648285",
+    "url": "/bitcoin-react/static/js/main.0bc1ad97.chunk.js"
+  },
+  {
+    "revision": "19f2c39af82577ad8e52",
+    "url": "/bitcoin-react/static/js/runtime-main.3cbfbf8c.js"
+  }
+]);
